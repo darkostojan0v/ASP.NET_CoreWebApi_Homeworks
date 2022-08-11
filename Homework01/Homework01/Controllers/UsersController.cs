fbb56592d42c8f641fe7cdc223217e4513c29b0e
darkostojan0v <95142705+darkostojan0v@users.noreply.github.com>
2022-08-11 21:45:19 +0200

    Homework01

---
+++ Homework01/Homework01/Controllers/UsersController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> AllUsers()
+        {
+            return Ok(StaticDb.UserNames);
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<string> GetByIndex(int index)
+        {
+            try
+            {
+                if(index < 0)
+                {
+                    return BadRequest("The index can not be negative value!");
+                }
+
+                if(index >= StaticDb.UserNames.Count)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, $"There is no resource on index {index}");
+                }
+
+                return Ok(StaticDb.UserNames[index]);
+            }
+            catch (Exception e)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred, please contact the admin!");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult AddUser()
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(Request.Body))
+                {
+                    string newUser = reader.ReadToEnd();
+
+                    if (string.IsNullOrEmpty(newUser))
+                    {
+                        return BadRequest("The body of the request can not be empty");
+                    }
+
+                    StaticDb.UserNames.Add(newUser);
+                    return StatusCode(StatusCodes.Status201Created, "The new user was added");
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred, please contact the admin!");
+            }
+        }
+    }
+}
---
+++ Homework01/Homework01/StaticDb.cs
@@ -0,0 +1,13 @@
+﻿namespace Homework01
+{
+    public static class StaticDb
+    {
+        public static List<string> UserNames = new List<string>
+        {
+            "Stojanov Darko",
+            "Georgievski Stefan",
+            "Ilievski Viktor"
+        };
+
+    }
+}
